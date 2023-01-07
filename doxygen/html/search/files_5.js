79376b56d8a2c0e15411c1dae5f631c7b0bb84ff
henrikerch <henri.kerch@student.uantwerpen.be>
2023-01-07 23:42:08 +0100

    committing before testing on ubuntu

--- doxygen/html/search/files_5.js
+++ doxygen/html/search/files_5.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['menustate_2ecpp_0',['MenuState.cpp',['../_menu_state_8cpp.html',1,'']]],
-  ['menustate_2eh_1',['MenuState.h',['../_menu_state_8h.html',1,'']]],
-  ['model_2ecpp_2',['Model.cpp',['../_model_8cpp.html',1,'']]],
-  ['model_2eh_3',['Model.h',['../_model_8h.html',1,'']]]
+  ['main_2ecpp_0',['main.cpp',['../main_8cpp.html',1,'']]],
+  ['menustate_2ecpp_1',['MenuState.cpp',['../_menu_state_8cpp.html',1,'']]],
+  ['menustate_2eh_2',['MenuState.h',['../_menu_state_8h.html',1,'']]],
+  ['model_2ecpp_3',['Model.cpp',['../_model_8cpp.html',1,'']]],
+  ['model_2eh_4',['Model.h',['../_model_8h.html',1,'']]]
 ];
--- doxygen/html/search/functions_7.js
+++ doxygen/html/search/functions_7.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['maketexts_0',['makeTexts',['../class_menu_state.html#ad1e183b06e9057451f953e819f90757d',1,'MenuState']]],
-  ['menustate_1',['MenuState',['../class_menu_state.html#a53596dbe15ca27c230392927916e08fc',1,'MenuState']]],
-  ['model_2',['Model',['../classown_model_1_1_model.html#a1a3b3491bafc171de7eae06141aaaac1',1,'ownModel::Model']]],
-  ['movescreen_3',['moveScreen',['../class_camera.html#a3275d740a066280bb01ed26540dfa684',1,'Camera::moveScreen()'],['../class_level_state.html#ade49de9e4c7e81e56afab021b92df9d3',1,'LevelState::moveScreen()']]],
-  ['movescreenateighty_4',['moveScreenAtEighty',['../class_camera.html#a6ee788e6ae154e9f101cb95b36a6d367',1,'Camera']]]
+  ['main_0',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
+  ['maketexts_1',['makeTexts',['../class_menu_state.html#ad1e183b06e9057451f953e819f90757d',1,'MenuState']]],
+  ['menustate_2',['MenuState',['../class_menu_state.html#a53596dbe15ca27c230392927916e08fc',1,'MenuState']]],
+  ['model_3',['Model',['../classown_model_1_1_model.html#a1a3b3491bafc171de7eae06141aaaac1',1,'ownModel::Model']]],
+  ['movescreen_4',['moveScreen',['../class_camera.html#a3275d740a066280bb01ed26540dfa684',1,'Camera::moveScreen()'],['../class_level_state.html#ade49de9e4c7e81e56afab021b92df9d3',1,'LevelState::moveScreen()']]],
+  ['movescreenateighty_5',['moveScreenAtEighty',['../class_camera.html#a6ee788e6ae154e9f101cb95b36a6d367',1,'Camera']]]
 ];
